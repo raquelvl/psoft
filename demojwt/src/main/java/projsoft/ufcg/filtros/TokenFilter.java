f3374011eaea4b205bb0af50984ae662f9a443b0
Raquel Lopes <raquel@computacao.ufcg.edu.br>
2019-09-24 17:34:20 -0300

    Update TokenFilter.java
    melhoria do filtro

--- demojwt/src/main/java/projsoft/ufcg/filtros/TokenFilter.java
+++ demojwt/src/main/java/projsoft/ufcg/filtros/TokenFilter.java
@@ -31,23 +31,23 @@ public class TokenFilter extends GenericFilterBean {
 		String header = req.getHeader("Authorization");
 
 		if (header == null || !header.startsWith("Bearer ")) {
-			throw new ServletException("Token inexistente ou mal formatado!");
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED,
+					"Token inexistente ou mal formatado!");
+			return;
+			// throw new ServletException("Token inexistente ou mal formatado!");
 		}
 
 		// Extraindo apenas o token do cabecalho (removendo o prefixo "Bearer ").
 		String token = header.substring(TOKEN_INDEX);
 		try {
 			Jwts.parser().setSigningKey("login do batman").parseClaimsJws(token).getBody();
-		} catch(SignatureException | ExpiredJwtException | MalformedJwtException | PrematureJwtException | UnsupportedJwtException | IllegalArgumentException e) {
-			
-			//aqui optamos por tratar todas as exceções que podem ser lançadas da mesma forma e simplesmente
-			//repassar a mensagem de erro
-			//se quiser enviar mensagens em portugues mais personalizadas teria que capturar exceção
-			//por exceção
-            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
-            return;//a requisição nem precisa passar adiante, retornar já para o cliente pois não pode prosseguir daqui pra frente
-            //por falta de autorização
-        }
+		} catch (SignatureException | ExpiredJwtException | MalformedJwtException | PrematureJwtException
+				| UnsupportedJwtException | IllegalArgumentException e) {
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+			return;// a requisição nem precisa passar adiante, retornar já para o cliente pois não
+					// pode prosseguir daqui pra frente
+			// por falta de autorização
+		}
 
 		chain.doFilter(request, response);
 	}
