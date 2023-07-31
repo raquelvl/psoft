af013bf703aaba56ce1bf2fb585170d09a19265a
Raquel Lopes <raquel@computacao.ufcg.edu.br>
2023-07-31 08:55:22 -0300

    31/07/2023 2023.1 3 Builder 2

--- estoquez/src/main/java/primeiro/exemplo/estoquez/excecoes/HandlerDeExcecoes.java
+++ estoquez/src/main/java/primeiro/exemplo/estoquez/excecoes/HandlerDeExcecoes.java
@@ -14,7 +14,6 @@ public class HandlerDeExcecoes {
 
 	@ExceptionHandler(ProdutoInvalidoException.class)
 	public ResponseEntity<DetalhesDoProblema> lidaComProdutoInvalidoException(ProdutoInvalidoException pie) {
-		System.out.println("1");
 		DetalhesDoProblema problema = DetalhesDoProblema
 				.builder()
 				.status(HttpStatus.BAD_REQUEST.value())
@@ -28,7 +27,6 @@ public class HandlerDeExcecoes {
 
 	@ExceptionHandler(ProdutoJaExisteException.class)
 	public ResponseEntity<DetalhesDoProblema> lidaComProdutoJaExisteException(ProdutoJaExisteException pjee) {
-		System.out.println("2");
 		DetalhesDoProblema problema = DetalhesDoProblema
 				.builder()
 				.status(HttpStatus.BAD_REQUEST.value())
@@ -41,7 +39,6 @@ public class HandlerDeExcecoes {
 	
 	@ExceptionHandler(CampoInvalidoException.class)
 	public ResponseEntity<DetalhesDoProblema> lidaComCampoInvalidoException(CampoInvalidoException cie) {
-		System.out.println("3");
 		DetalhesDoProblema problema = new DetalhesDoProblema();
 		problema.setStatus(HttpStatus.BAD_REQUEST.value());
 		problema.setTitle(cie.getTitulo());
@@ -52,7 +49,6 @@ public class HandlerDeExcecoes {
 	
 	@ExceptionHandler(ProdutoNaoEncontradoException.class)
 	public ResponseEntity<DetalhesDoProblema> lidaComProdutoNaoEncontradoException(ProdutoNaoEncontradoException pnee) {
-		System.out.println("4");
 		DetalhesDoProblema problema = new DetalhesDoProblema();
 		problema.setStatus(HttpStatus.NOT_FOUND.value());
 		problema.setTitle(pnee.getTitulo());
@@ -63,7 +59,6 @@ public class HandlerDeExcecoes {
 	
 	@ExceptionHandler(NovoValorInvalidoException.class)
 	public ResponseEntity<DetalhesDoProblema> lidaComNovoValorInvalidoException(NovoValorInvalidoException nvie) {
-		System.out.println("5");
 		DetalhesDoProblema problema = new DetalhesDoProblema();
 		problema.setStatus(HttpStatus.BAD_REQUEST.value());
 		problema.setTitle(nvie.getTitulo());
